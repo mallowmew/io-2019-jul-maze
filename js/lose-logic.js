e962e10c0fed4b991a9f889be2b82b267e7638fa
Rachmann Joubert <rachmannjoubert@gmail.com>
2019-08-20 15:50:54 +0100

    declared variables

---
+++ js/lose-logic.js
@@ -0,0 +1,12 @@
+var brushes = document.querySelectorAll('.brush')
+
+var tail = document.getElementById('tail')
+
+var mazeBackground = document.getElementById('maze-background')
+
+
+function loseLogic() {
+    mazeBackground.innerHTML = '<div class="dummy-popup">ksjdhfkjsadfh</div>'
+}
+
+
