90849dae0d013e38dd0e1430329f4aef2262fd6a
Cat <blackcatstone@gmail.com>
2019-08-20 11:53:01 +0100

    Remove unnecessary console log

--- js/splash.js
+++ js/splash.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     $('#play-button').on('click', function () {
         $('.splash').remove()
         $('.container').load('./partials/game.html')
-        console.log('New page loaded')
     })
 })
 
