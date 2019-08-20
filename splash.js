996f9c1fa78e0b8366481bd7df9083e2404180a6
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-08-20 10:00:40 +0100

    Fixed some syntax

--- splash.js
+++ splash.js
@@ -1,4 +1,4 @@
-document.getElementById(play-button).addEventListener("click", function () {
+document.getElementById('play-button').addEventListener("click", function () {
     $('.splash').remove()
     $('.container').load('game.html')
 })
