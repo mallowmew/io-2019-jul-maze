bd247b9c1c72605990f87ad63cca176b9ecec47c
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-08-21 10:26:21 +0100

    Updated popup code

--- js/popups.js
+++ js/popups.js
@@ -1,6 +1,15 @@
-$(document).ready(function () {
-    $(".endzone")
-    $('.popup-container').load('./partials/popups.html')
-    console.log('dsgasgrege')
+
+var showPopup = new CustomEvent(
+    'showPopup', {
+        bubbles: false,
+        cancelable: true,
+        detail: {}
+    }
+)
+
+document.getElementById('hind-img').addEventListener('mouseover', function () {
+    document.dispatchEvent(showPopup)
+    //$('.popup-container').load('./partials/popups.html')
+    console.log('!')
 })
 
