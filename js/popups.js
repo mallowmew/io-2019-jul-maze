1445ebfcfffd94f5de922af38062d0128642d25a
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-08-21 15:38:45 +0100

    Fixed lose popup

--- js/popups.js
+++ js/popups.js
@@ -1,22 +1,17 @@
 
-// document.querySelector('.end-zone').addEventListener('mouseover', function () {
-//     $('.show-popup').show()
-//     $('.show-popup').load('./partials/popups.html', {}, function(responseText, textStatus, jqXHR) {
-//         document.querySelector('#pop-up-msg').textContent = "You win!"
-//     })
-// })
 
-$(document).on('gameWin', function(){
+$(document).on('gameWin', function() {
     $('.show-popup').show()
-    $('.show-popup').load('./partials/popups.html', {}, function(responseText, textStatus, jqXHR) {
+    $('.show-popup').load('./partials/popups.html', {}, function (responseText, textStatus, jqXHR) {
         document.querySelector('#pop-up-msg').textContent = "You win!"
+    })
 })
 
-// $('.lose').on('gameLose', function(){
-//     $('.show-popup').show()
-//     $('.show-popup').load('./partials/popups.html', {}, function(responseText, textStatus, jqXHR) {
-//         $("#bone-img").attr({ "src": "images/lose-bone.svg" });
-//         $('.pop-up-background').css('background-color','#f9f871')
-//         document.querySelector('#pop-up-msg').textContent = "You lose!"
-//     })
-// })
+$(document).on('gameLose', function() {
+    $('.show-popup').show()
+    $('.show-popup').load('./partials/popups.html', {}, function(responseText, textStatus, jqXHR) {
+        $("#bone-img").attr({ "src": "images/lose-bone.svg" });
+        $('.pop-up-background').css('background-color','#f9f871')
+        document.querySelector('#pop-up-msg').textContent = "You lose!"
+    })
+})
