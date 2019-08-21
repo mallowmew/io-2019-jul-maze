28bf0d6fa90984bcf26eb13701d98c4bd14340b4
Matthew Hammond <matthew.ian.hammond@gmail.com>
2019-08-21 11:28:11 +0100

    Created a seperate instance of endscreen for loosing

--- js/popups.js
+++ js/popups.js
@@ -1,5 +1,5 @@
 
-var showPopup = new CustomEvent(
+var showWinPopup = new CustomEvent(
     'showPopup', {
         bubbles: false,
         cancelable: true,
@@ -8,7 +8,20 @@ var showPopup = new CustomEvent(
 )
 
 document.querySelector('.end-zone').addEventListener('mouseover', function () {
-    document.dispatchEvent(showPopup)
+    document.dispatchEvent(showWinPopup)
+    $('.game').load('./partials/popups.html')
+})
+
+var showLosePopup = new CustomEvent(
+    'showPopup', {
+        bubbles: false,
+        cancelable: true,
+        detail: {}
+    }
+)
+
+document.querySelector('.lose').addEventListener('mouseover', function () {
+    document.dispatchEvent(showLosePopup)
     $('.game').load('./partials/popups.html')
 })
 
