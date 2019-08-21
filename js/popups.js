d11e7c483b4ffe5011b044e4a8149d0abadc32db
Cat <blackcatstone@gmail.com>
2019-08-21 15:50:42 +0100

    Remove unnecessary params

--- js/popups.js
+++ js/popups.js
@@ -1,13 +1,13 @@
 $(document).on('gameWin', function() {
     $('.show-popup').show()
-    $('.show-popup').load('./partials/popups.html', {}, function (responseText, textStatus, jqXHR) {
+    $('.show-popup').load('./partials/popups.html', {}, function () {
         $('#pop-up-msg').text("You win!")
     })
 })
 
 $(document).on('gameLose', function() {
     $('.show-popup').show()
-    $('.show-popup').load('./partials/popups.html', {}, function(responseText, textStatus, jqXHR) {
+    $('.show-popup').load('./partials/popups.html', {}, function() {
         $("#bone-img").attr({ "src": "images/lose-bone.svg" });
         $('.pop-up-background').css('background-color','#f9f871')
         $('#pop-up-msg').text("You lose!")
