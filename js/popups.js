8de6d5e98b8ab73f2d56c9d6c255f421cc482e98
Cat <blackcatstone@gmail.com>
2019-08-21 15:55:05 +0100

    Change alt text

--- js/popups.js
+++ js/popups.js
@@ -8,7 +8,8 @@ $(document).on('gameWin', function() {
 $(document).on('gameLose', function() {
     $('.show-popup').show()
     $('.show-popup').load('./partials/popups.html', {}, function() {
-        $("#bone-img").attr({ "src": "images/lose-bone.svg" });
+        $("#bone-img").attr({ "src": "images/lose-bone.svg", 
+                            "alt": "Lose popup background"});
         $('.pop-up-background').css('background-color','#f9f871')
         $('#pop-up-msg').text("You lose!")
     })
