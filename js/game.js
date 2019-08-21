97e910ad6997c8adcf0c2a5cc09c65fbac1272ce
Cat <blackcatstone@gmail.com>
2019-08-21 16:28:33 +0100

    Change alignment of attrs

--- js/game.js
+++ js/game.js
@@ -83,8 +83,10 @@ $(document).on('gameWin', function() {
 $(document).on('gameLose', function() {
     $('.show-popup').show()
     $('.show-popup').load('./partials/popups.html', {}, function() {
-        $("#bone-img").attr({ "src": "images/lose-bone.svg", 
-                            "alt": "Lose popup background"});
+        $("#bone-img").attr({ 
+            "src": "images/lose-bone.svg", 
+            "alt": "Lose popup background"
+        });
         $('.pop-up-background').css('background-color','#f9f871')
         $('#pop-up-msg').text("You lose!")
         $('#pop-up-msg').css('color', '#f9f871')
