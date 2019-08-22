874438925799b50f2f6bd11a871b932199c7e840
Cat <blackcatstone@gmail.com>
2019-08-22 11:27:31 +0100

    Remove gameStart custom event Feedback from senior dev suggested that using custom events would be problematic in the sprint review.

--- js/game.js
+++ js/game.js
@@ -1,42 +1,34 @@
-var gameStart = new CustomEvent(
-    'gameStart', {
-        bubbles: false,
-        cancelable: true,
-        detail: {}
-    }
-)
+var gameStarted = false
 
 $('.start-zone').each(function(index, starts) {
     starts.addEventListener('mouseover', function () {
-        $(document).trigger('gameStart')
-    })
-})
-
-$(document).on('gameStart', function() {
-    $('.lose').each( function(index, hitboxes) {
-        hitboxes.addEventListener('mouseenter', function() {
-            $('.show-popup').show()
-            $('.show-popup').load('partials/popups.html', {}, function() {
-                $("#bone-img").attr({ 
-                    "src": "images/lose-bone.svg", 
-                    "alt": "Lose popup background"
-                });
-                $('.pop-up-background').css('background-color','#f9f871')
-                $('#pop-up-msg').text("You lose!")
-                $('#pop-up-msg').css('color', '#f9f871')
+        if (!gameStarted) {
+            $('.lose').each( function(index, hitboxes) {
+                hitboxes.addEventListener('mouseenter', loseGame)
             })
-        })
-    })
-}) 
-
-$(document).on('gameStart', function() {
-    $('.end-zone').on('mouseover', function () {
-        $('.show-popup').show()
-        $('.show-popup').load('partials/popups.html', {}, function () {
-            $('#pop-up-msg').text("You win!")
-        })
+            $('.end-zone').on('mouseover', winGame)
+            gameStarted = true;
+        }
     })
 })
+
+function loseGame() {
+    $('.show-popup').show().load('partials/popups.html', {}, function() {
+        $("#bone-img").attr({ 
+            "src": "images/lose-bone.svg", 
+            "alt": "Lose popup background"
+        });
+        $('.pop-up-background').css('background-color','#f9f871')
+        $('#pop-up-msg').text("You lose!").css('color', '#f9f871')
+    })
+}
+
+function winGame() {
+    $('.show-popup').show()
+    $('.show-popup').load('partials/popups.html', {}, function () {
+        $('#pop-up-msg').text("You win!")
+    })
+}
 
 $(function() {
     function firstBrushLoop() {
